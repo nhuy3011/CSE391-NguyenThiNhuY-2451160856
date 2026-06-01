d18ea27bb1309b75d0b939e4d0f11826f92ada0d
Nguyen Thi Nhu Y <nhuynguyen301126@gmai.com>
2026-06-01 13:35:05 +0700

    Hoan thanh chay react dau tien

---
+++ hello-react/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+      <div>
+          <h1>Nguyễn Thị Như Ý - TLU</h1>
+          <p>Hôm nay là một ngày tuyệt vời để học React!</p>
+          <ul>
+              <li>HTML & CSS</li>
+              <li>JavaScript nâng cao</li>
+              <li>React Basics</li>
+          </ul>
+      </div>
+  );
+}
+export default App;
